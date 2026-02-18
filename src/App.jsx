7e657a1789b41e28302c158b6e3da7628a2f18c6
Aryan <aryancdry5@gmail.com>
2026-02-18 07:19:09 +0545

    adding emails

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 
-import { BrowserRouter,Routes, Route } from "react-router-dom"
-import { useState } from "react";
-import PrivateRoutes from "./routes/PrivateRoutes";
-import PublicRoutes from "./routes/PublicRoutes";
-import './App.css'
+// import { BrowserRouter,Routes, Route } from "react-router-dom"
+// import { useState } from "react";
+// import PrivateRoutes from "./routes/PrivateRoutes";
+// import PublicRoutes from "./routes/PublicRoutes";
+// import './App.css'
+
+import Login from "./core/Auth/Login";
 
 function App() {
 //const[isAuthenticated] = useState(localStorage.getItem("token") ? true : false);
   return (
-    <BrowserRouter>
-{isAuthenticated ? <PrivateRoutes/> : <PublicRoutes/>}
-  
-   </BrowserRouter>
+//     <BrowserRouter>
+// {isAuthenticated ? <PrivateRoutes/> : <PublicRoutes/>}
+<Login/>  
+//    </BrowserRouter>
   )
 }
 

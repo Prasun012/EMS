5f1401ae062ad953da0600cd557e848cb2fe5bb7
lucky-shahi <luckyshahi7907@gmail.com>
2026-02-18 07:19:55 +0545

    accepting current change

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,7 @@ import PrivateRoutes from "./core/Routes/PrivateRoutes";
 // import PublicRoutes from "./core/Routes/PublicRoutes";
 
 function App() {
-<<<<<<< HEAD
 // const[isAuthenticated] = useState(localStorage.getItem("token") ? true : false);
-=======
-//const[isAuthenticated] = useState(localStorage.getItem("token") ? true : false);
->>>>>>> a8cee5f3592516cf5255736c69b5ffee8b7856b0
   return (
     <BrowserRouter>
 {/* {isAuthenticated ? <PrivateRoutes/> : <PublicRoutes/>} */}

fa9cefbb1d003581a26b8f2333bdc7eb6a25e981
lucky-shahi <luckyshahi7907@gmail.com>
2026-02-18 07:58:00 +0545

    signup update

--- src/UserData/Users.js
+++ src/UserData/Users.js
@@ -2,6 +2,7 @@ const Users = [
     { id: 1, email: "ram@example.com", username: "Ram", password: "123456" },
     { id: 2, email: "p1@example.com", username: "P1", password: "asdfghjkl" },
 ];
+// registeration
 export let users = [];
 export const addUser = (newUser) => {
     users.push(newUser);

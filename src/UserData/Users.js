71da3a9338718a06d4221e75048a4d19a41eb1e7
lucky-shahi <luckyshahi7907@gmail.com>
2026-02-19 20:04:16 +0545

    trial

--- src/UserData/Users.js
+++ src/UserData/Users.js
@@ -4,23 +4,13 @@ let Users = [
     { id: 1, email: "ram@example.com", username: "Ram", password: "123456", role: "superadmin" },
     { id: 2, email: "p1@example.com", username: "P1", password: "asdfghjkl", role: "user" },
 ];
-<<<<<<< HEAD
-
-export const addUser = (user) => {
-    Users.push({
-        id: Users.length + 1,
-        role: "user", // default role
-        ...user
-    });
-=======
 //  registeration
- export let users = [];
- export const addUser = (newUser) => {
-     users.push(newUser);
- };
- export const getUsers = () => {
-     return users;
->>>>>>> 66bfa559fb20fc918fd2cb1e3539bb2e6d462788
+export let users = [];
+export const addUser = (newUser) => {
+    users.push(newUser);
+};
+export const getUsers = () => {
+    return users;
 };
 
 export default Users;
